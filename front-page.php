8a5e00bd9eb19265655573344213a1a68a399d11
Lars EriK <Lars.e.faber@gmail.com>
2020-05-20 13:25:40 +0200

    updated css and frontpage

--- front-page.php
+++ front-page.php
@@ -20,29 +20,44 @@ get_header();
 <main id="site-content" role="main">
 
     <section class="section-middle">
-    <?php
+        <div class="frontpage-main-thread">
+            <?php
+            $homepagePosts = new WP_Query(array(
+                'posts_per_page' => 10,
+                'post_type' => 'post',
+                'orderby' => 'date',
+                'order' => 'DESC',
+                'paged' => $paged
+            ));
 
-    $homepagePosts = new WP_Query(array(
-        'posts_per_page' => 10,
-        'post_type' => 'post',
-        'orderby' => 'date',
-        'order' => 'DESC',
-        'paged' => $paged
-    ));
-    
-    if ( $homepagePosts->have_posts() ) : 
-        while ( $homepagePosts->have_posts() ) : $homepagePosts->the_post();?>
-        
-        <a href="<?php echo the_permalink() ?>"><div>
-            <img><?php echo the_post_thumbnail() ?></img>
-            <h2><?php echo the_title() ?></h2>
-            <p></p>
-        </div></a>
-        
-        <?endwhile; 
-    endif; 
+            if ( $homepagePosts->have_posts() ) : 
+                while ( $homepagePosts->have_posts() ) : $homepagePosts->the_post();?>
+                
+                <a href="<?php echo the_permalink() ?>">
+                    <div class="frontpage-main-boxes">
+                        <?php //echo wp_get_attachment_image_srcset(get_post_thumbnail_id())?>
+                        <!-- category boxes -->
+                        <div class="category-boxes">
+                            <?php
+                            $post_categories = wp_get_post_categories(get_the_ID());
+                            foreach ($post_categories AS $cat_id) {
+                                $cat = get_category( $cat_id );?>
 
-    ?>
+                                <div class="<?php echo 'post-category-' . $cat_id; ?>"><?php echo $cat->name; ?></div>
+                            <?php } ?>
+                        </div>
+
+                        <div class="box-image"><?php echo the_post_thumbnail('medium_large'); ?></div>
+                        <h2><?php echo the_title(); ?></h2>
+                    </div>
+                </a>
+                
+                <?endwhile; 
+            endif;
+            ?>
+        </div>
+        <div class="fontpage-minor-thread">
+        </div>
     </section>
 
 </main><!-- #site-content -->
