4c09d324fe271963f569121fa00451b9af42a956
Lars EriK <Lars.e.faber@gmail.com>
2020-06-28 18:53:05 +0200

    added css breakpoint variables and mixins, and fixed mail-animation.js

--- js/mail-animation.js
+++ js/mail-animation.js
@@ -1,7 +1,12 @@
 // mail graphic animation -> on hover -> play until end by adding and removing a class
 
 jQuery(document).ready(function($) {
-    $(".header-tips-oss").bind("webkitAnimationEnd mozAnimationEnd animationend", function(){ $("#tips-oss-img").removeClass("mail-animation") })  
-    $(".header-tips-oss").hover(function(){ $("#tips-oss-img").addClass("mail-animation"); })
+    $(".header-tips-oss").bind("webkitAnimationEnd mozAnimationEnd animationend", () => {
+        $("#tips-oss-img").removeClass("mail-animation")
+    })
+
+    $(".header-tips-oss").mouseenter(() => {
+        $("#tips-oss-img").addClass("mail-animation");
+    })
 });
 
