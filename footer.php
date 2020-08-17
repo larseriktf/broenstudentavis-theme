e8421554f52627621e4d10c66b8900d25fa03912
Lars EriK <Lars.e.faber@gmail.com>
2020-08-17 19:36:18 +0200

    ferdig med footer, andreutkast

--- footer.php
+++ footer.php
@@ -15,19 +15,31 @@
             <footer id="site-footer" role="contentinfo" class="header-footer-group">
                 <div class="footer-showcase">
                     <img id="footer-showcase-photo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/photos/photos_hiof_bygning.JPG" alt="hiof">
+                    <div class="footer-showcase-item">
+                        <h2>Vil du jobbe i Broen?</h2>
+                        <p>Er du en student ved Høgskolen i Østfold som ønsker å delta i noe større?</p>
+                        <a href="mailto:Broen@broenstudentavis.no?subject=Søknad%20til%20stilling" class="button-round-blue">TA KONTAKT: broen@broenstudentavis.no</a>
+                    </div>
                 </div>
                 <div class="footer-content">
                     <div class="footer-column">
                         <h3>Om oss</h3>
                         <p>Broen er en studentavis ved  Høgskolen i Østfold den ble lansert 3. Februar 2020.</p>
-                        <p>Broen Studentavis har som mål å gi Studentene ved HiØ en fri taleplattform for å uttrykke meningene, sakene, verdiene og idealene våre.</p>
-                        <p>Broen Studentavis følger retningslinjene for pressefrihet, ytringsfrihet, tale- og informasjonsfrihet stipulert i Vær varsom plakaten.</p>
+                        <p>
+                        <a href="<?php echo get_template_directory_uri() . '/om-oss/' ?>">Les mer</a>
+                        </p>
                     </div>
                     <div class="footer-column">
+                        <h3>Redaktør, Daglig Leder</h3>
+                        <p>Kawser Mudey</p>
+                        <h3>Redaktør</h3>
+                        <p>Sarah Lunner</p>
                     </div>
                     <div class="footer-column">
-                    </div>
-                    <div class="footer-column">
+                        <h3>Kontakt oss</h3>
+                        <p>
+                            <a href="mailto:broen@broenstudentavis.no">broen@broenstudentavis.no</a>
+                        </p>
                     </div>
                 </div>
 
